ad766bb30553a69f6e6fe6aa24d0e4cf2a19db6a
PravallikaGit23 <mail2pravs23@gmail.com>
2026-02-24 19:11:05 +0530

    commenting some content

--- springcore/src/main/java/org/springioc/Mobile.java
+++ springcore/src/main/java/org/springioc/Mobile.java
@@ -5,23 +5,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Mobile {
     public static void main(String[] args) {
-        Airtel airtel = new Airtel();
-        airtel.calling();
-        airtel.data();
-
-        //runtime polymorphism
-        Sim sim = new Vodafone();
-        sim.calling();
-        sim.data();
-
-        // This is a container object - read the config file and create beans.
+//        Airtel airtel = new Airtel();
+//        airtel.calling();
+//        airtel.data();
+//
+//        //runtime polymorphism
+//        Sim sim = new Vodafone();
+//        sim.calling();
+//        sim.data();
+//
+//        // This is a container object - read the config file and create beans.
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-        //getBean returns an Object so we need to cast to the required Object
-        Airtel airtelsim = (Airtel) context.getBean("airtel");
-        airtelsim.calling();
-        airtelsim.data();
-        //System.out.println("airtel bean called.");
+//
+//        //getBean returns an Object so we need to cast to the required Object
+//        Airtel airtelsim = (Airtel) context.getBean("airtel");
+//        airtelsim.calling();
+//        airtelsim.data();
+//        //System.out.println("airtel bean called.");
 
         Sim simContext = context.getBean("sim",Sim.class);
         simContext.calling();
